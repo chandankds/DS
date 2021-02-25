3b95804db09d3b6fdca7e8fb8af4e5598bd6d9e5
chandan singh <chandankds@gmail.com>
2021-02-25 10:50:56 +0530

    Unique is done

--- ArraysAndStrings/Unique.cpp
+++ ArraysAndStrings/Unique.cpp
@@ -1,6 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+//using the hashMap
+bool unique(string str){
+	map<char, int> m;
+	if(str.length() <= 0){
+		cout << "string is empty doing me crazy" << endl;
+		return true;	
+	}
+	int len = str.length();
+	for(int i=0; i<len; i++){
+		m[str[i]]++;
+		for(auto j = m.begin(); j != m.end(); j++){
+			if(j->second > 1){
+				return false;
+			}
+		}	
+	}
+	return true;
+}
+// using the boolian vector
+bool unique2(string str){
+	int len = str.length();
+	bool arr[25] = {false};
+	for(int i =0; i<len; i++){
+		int temp = str[i] - 'a';
+		if(arr[temp]){
+			return false;
+		}else
+			arr[temp] = true;
+	}
+	return true;
+}
+
+//using the bit vector called bitset
+bool unique3(string str){
+	bitset<26> bset;
+	int len = str.length();
+	for(int i = 0; i<len; i++){
+		if(bset.test(str[i] - 'a')){
+			cout << bset << endl;
+			return false;
+		}
+		bset[str[i] - 'a'] = 1;
+	}
+	cout << bset << endl;
+	return true;
+}
+
 int main(){
-	cout << "Long journey ahed" << endl;
+	string str;
+	cout << "Enter the strig " << endl;
+	cin >> str;
+	if(unique3(str))
+		cout << "String is unique" << endl;
+	else
+		cout << "String is not unique" << endl;
 	return 0;
 }
